7c2f6e2dd5bf7ca01a310fc7ee769bbbb6318407
Enzo <enzoastorga98@gmail.com>
2024-04-04 20:56:45 -0300

    Agrego Clase 1 de programación III completa

--- Enzo-Astorga/Java/Lección1/MundoPc/src/ar/com/system2024/mundopc/Orden.java
+++ Enzo-Astorga/Java/Lección1/MundoPc/src/ar/com/system2024/mundopc/Orden.java
@@ -20,6 +20,18 @@ public class Orden {
         if(this.contadorComputadora < Orden.MAX_COMPUTADORAS){
             this.computadora[this.contadorComputadora++] = computadora;
         }
+        else{
+            System.out.println("Has superado el límite: "+Orden.MAX_COMPUTADORAS);
+        }
+    }
+    
+    // Mostrar Orden
+    public void mostrarOrden(){
+        System.out.println("Orden #: "+this.idOrden);
+        System.out.println("Computadoras de la orden #: "+this.idOrden);
+        for(int i = 0; i < this.contadorComputadora; i++){
+            System.out.println(this.computadora[i]);
+        }
     }
     
     
