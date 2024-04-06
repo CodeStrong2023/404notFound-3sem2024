f5b2be442f86a174c528befa369e0577ff79ef10
celeste pulitta <136055066+celestepulitta2023@users.noreply.github.com>
2024-04-06 18:05:43 -0300

    Agrego la ultima parte de la clase

--- MundoPc/src/mundopc/mundoPC.java
+++ MundoPc/src/mundopc/mundoPC.java
@@ -29,6 +29,65 @@ public class mundoPC {
         
         // Crear más objetos de tipo computadora con todos sus elementos.
         // Completar una lista en el objeto orden1 que llegue a los 10 elementos.
-        // Probar de ésta manera los métodos al máximo rendimiento.
+        // Probar de ésta manera los métodos al máximo rendimiento.// 
+        
+        //Marca Dell
+        Monitor monitorDell = new Monitor("Dell", 20);
+        Teclado tecladoDell = new Teclado("Bluetooth", "Dell");
+        Raton ratonDell = new Raton("Bluetooth", "Dell");
+        Computadora computadoraDell = new Computadora("Computadora Dell", monitorDell, tecladoDell, ratonDell);
+        
+        // Marca Lenovo
+        Monitor monitorLenovo = new Monitor("Lenovo", 18);
+        Teclado tecladoLenovo = new Teclado("Bluetooth", "Lenovo");
+        Raton ratonLenovo = new Raton("Bluetooth", "Lenovo");
+        Computadora computadoraLenovo = new Computadora("Computadora Lenovo", monitorLenovo, tecladoLenovo, ratonLenovo);
+        
+        // Marca Apple
+        Monitor monitorApple = new Monitor("Apple", 22);
+        Teclado tecladoApple = new Teclado("Bluetooth", "Apple");
+        Raton ratonApple = new Raton("Bluetooth", "Apple");
+        Computadora computadoraApple = new Computadora("Computadora Apple", monitorApple, tecladoApple, ratonApple);
+        
+        // Marca Asus
+        Monitor monitorAsus = new Monitor("Asus", 20);
+        Teclado tecladoAsus = new Teclado("Bluetooth", "Asus");
+        Raton ratonAsus = new Raton("Bluetooth", "Asus");
+        Computadora computadoraAsus = new Computadora("Computadora Asus", monitorAsus, tecladoAsus, ratonAsus);
+        
+        // Marca Acer
+        Monitor monitorAcer = new Monitor("Acer", 18);
+        Teclado tecladoAcer = new Teclado("Bluetooth", "Acer");
+        Raton ratonAcer = new Raton("Bluetooth", "Acer");
+        Computadora computadoraAcer = new Computadora("Computadora Acer", monitorAcer, tecladoAcer, ratonAcer);
+        
+        // Marca Samsung
+        Monitor monitorSamsung = new Monitor("Samsung", 32);
+        Teclado tecladoSamsung = new Teclado("Bluetooth", "Samsung");
+        Raton ratonSamsung = new Raton("Bluetooth", "Samsung");
+        Computadora computadoraSamsung = new Computadora("Computadora Samsung", monitorSamsung, tecladoSamsung, ratonSamsung);
+        
+        // Marca Huawei
+        Monitor monitorHuawei = new Monitor("Huawei", 18);
+        Teclado tecladoHuawei = new Teclado("Bluetooth", "Huawei");
+        Raton ratonHuawei = new Raton("Bluetooth", "Huawei");
+        Computadora computadoraHuawei = new Computadora("Computadora Huawei", monitorHuawei, tecladoHuawei, ratonHuawei);
+        
+        // Marca MSI
+        Monitor monitorMSI = new Monitor("MSI", 30);
+        Teclado tecladoMSI = new Teclado("Bluetooth", "MSI");
+        Raton ratonMSI = new Raton("Bluetooth", "MSI");
+        Computadora computadoraMSI = new Computadora("Computadora MSI", monitorMSI, tecladoMSI, ratonMSI);
+        
+        orden1.agregarComputadora(computadoraDell);
+        orden1.agregarComputadora(computadoraLenovo);
+        orden1.agregarComputadora(computadoraApple);
+        orden1.agregarComputadora(computadoraAsus);
+        orden1.agregarComputadora(computadoraAcer);
+        orden1.agregarComputadora(computadoraSamsung);
+        orden1.agregarComputadora(computadoraHuawei);
+        orden1.agregarComputadora(computadoraMSI);
+        
+        orden1.mostrarOrden();
     }
 }
