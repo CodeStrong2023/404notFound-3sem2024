864ee0809e80c49837a3418e5c863209ca5aa317
Enzo <enzoastorga98@gmail.com>
2024-04-06 13:16:11 -0300

    Arreglo de pruebas parte 2

--- MundoPc/src/mundopc/mundoPC.java
+++ MundoPc/src/mundopc/mundoPC.java
@@ -14,5 +14,21 @@ public class mundoPC {
         Teclado tecladoGamer = new Teclado("Bluetooth", "Gamer");
         Raton ratonGamer = new Raton("Bluetooth", "Gamer");
         Computadora computadoraGamer = new Computadora("Computadora Gamer", monitorGamer, tecladoGamer, ratonGamer);
+        Orden orden1 = new Orden(); // Inicializamos el arreglo vacío.
+        Orden orden2 = new Orden(); // Una nueva lista para el objeto orden2.
+        orden1.agregarComputadora(computadoraHP);
+        orden1.agregarComputadora(computadoraGamer);
+        
+        Computadora computadorasVarias = new Computadora("Computadora de diferentes marcas", monitorHP, tecladoGamer, ratonHP);
+        orden2.agregarComputadora(computadorasVarias);
+        
+        orden1.mostrarOrden();
+        orden2.mostrarOrden();
+        
+        
+        
+        // Crear más objetos de tipo computadora con todos sus elementos.
+        // Completar una lista en el objeto orden1 que llegue a los 10 elementos.
+        // Probar de ésta manera los métodos al máximo rendimiento.
     }
 }
