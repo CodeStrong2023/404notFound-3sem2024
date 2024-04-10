d6f56e6fe2bd97eff9620d1f08d74d5a8cafecfe
maxi <maxirobilottapons@gmail.com>
2024-04-09 22:32:40 -0300

    Agrego mi parte MRO

--- python/semana 1/Leccion9/test_figura_geometrica.py
+++ python/semana 1/Leccion9/test_figura_geometrica.py
@@ -3,3 +3,8 @@ cuadrado1 = Cuadrado(5, 'Azul')
 print(cuadrado1.ancho)
 print(cuadrado1.alto)
 print(f'Cálculo del área del cuadrado: {cuadrado1.calcular_area()}' )
+
+# MRO = Method Resolution Order
+# Permite conocer la jearaquía de las clases frente a la clase actual en la que estamos llamando a un método.
+
+print(Cuadrado.mro())
