5005127b57d7102a7a3b7eac0b80e688d531a7e1
Guillermo Giannone <guillegiannone@gmail.com>
2024-04-09 23:17:13 -0300

    Agrego mi parte clase Rectangulo

--- python/semana 1/Leccion9/Cuadrado.py
+++ python/semana 1/Leccion9/Cuadrado.py
@@ -11,3 +11,5 @@ class Cuadrado(FiguraGeometrica, Color):
     def calcular_area(self):
         return self.alto * self.ancho
 
+    def __str__(self):
+        return f"{FiguraGeometrica.__str__(self)}{Color.__str__(self)}"
--- python/semana 1/Leccion9/test_figura_geometrica.py
+++ python/semana 1/Leccion9/test_figura_geometrica.py
@@ -8,3 +8,10 @@ print(f'Cálculo del área del cuadrado: {cuadrado1.calcular_area()}' )
 # Permite conocer la jearaquía de las clases frente a la clase actual en la que estamos llamando a un método.
 
 print(Cuadrado.mro())
+print(cuadrado1)
+
+rectangulo1 = Rectangulo(5,8,"Verde")
+print(f"Calculo del area del rectangulo: {rectangulo1.calcular_area()}")
+print(rectangulo1)
+print(cuadrado1)
+
