41fa1f387559e4f13bddb9b0f5b275e933e6664b
Enzo <enzoastorga98@gmail.com>
2024-04-11 16:48:24 -0300

    Mi parte, Manejo de Enumeraciones(enum)

--- Java/semana2/ArgumentosVariables/src/test/TestArgumentosVariables.java
+++ Java/semana2/ArgumentosVariables/src/test/TestArgumentosVariables.java
@@ -3,8 +3,16 @@ package test;
 
 public class TestArgumentosVariables {
     public static void main(String[] args) {
-        
+        imprimirNumeros(3, 4, 5);
+        imprimirNumeros(1, 2);
+        variosParametros("Juan","Perez", 7, 8, 9);
     }
+    
+    private static void variosParametros(String nombre, String apellido, int ...numeros) {
+        System.out.println("Nombre y apellido: " + nombre + " " + apellido);
+        imprimirNumeros(numeros);
+    }
+    
     private static void imprimirNumeros(int ...numeros){
         for (int i = 0; i < numeros.length; i++) {
             System.out.println("Elementos: "+numeros[i]);
@@ -12,8 +20,5 @@ public class TestArgumentosVariables {
         }
     }
     
-     private static void variosParametros(String nombre, String apellido, int ...numeros) {
-        System.out.println("Nombre y apellido: " + nombre + " " + apellido);
-        imprimirNumeros(numeros);
-    }
+     
 }
