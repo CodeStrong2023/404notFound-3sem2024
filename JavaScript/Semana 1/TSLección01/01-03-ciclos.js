6290b0e63dd2f61b0be68e6e024855d9fadb5c8f
Javier <javii07bj@gmail.com>
2024-04-10 21:27:40 -0300

    Agrego mi parte ciclo do while

--- JavaScript/Semana 1/TSLección01/01-03-ciclos.js
+++ JavaScript/Semana 1/TSLección01/01-03-ciclos.js
@@ -5,3 +5,11 @@ while(contador < 3){
     contador++;
 }
 console.log("Fin del ciclo while");
+
+//do while
+let conteo = 0;
+do{
+    console .log(conteo);
+    conteo++;
+}while(conteo < 3);
+console.log("Fin del ciclo do while");
