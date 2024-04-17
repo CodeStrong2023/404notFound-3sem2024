3debbb3adf691eaf2acfe126e4b39796f13d7dd8
Guillermo Giannone <guillegiannone@gmail.com>
2024-04-17 20:29:24 -0300

    Agrego mi parte Recorremos elementos de Arreglo JS

--- JavaScript/Semana 1/TSLección02/arreglosenjavascrip.js
+++ JavaScript/Semana 1/TSLección02/arreglosenjavascrip.js
@@ -2,3 +2,10 @@
 //Let autos= new Array('Ferrari', 'Renault', 'BMW'); Esta es la sintaxis vieja
 const autos = ['Ferrari', 'Renault', 'BMW'];
 console.log(autos);
+
+//Recorremos los elementos de un arreglo
+console.log(autos[0]);
+console.log(autos[2]);
+for(let i = 0; i < autos.length; i++){
+    console.log(i + ' : ' + autos[i]);
+}
