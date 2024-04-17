e35ae4b59175c296686d742e18649fda6c6fc88f
Diego Rafael Ramos Llanos <dr0573131@gmail.com>
2024-04-17 20:56:12 -0300

    agrego el video 3

--- JavaScript/Semana 1/TSLección02/arreglosenjavascrip.js
+++ JavaScript/Semana 1/TSLección02/arreglosenjavascrip.js
@@ -9,3 +9,19 @@ console.log(autos[2]);
 for(let i = 0; i < autos.length; i++){
     console.log(i + ' : ' + autos[i]);
 }
+
+// Modificamos los elementos del arreglo
+autos[1] = 'Volvo';
+console.log(autos[1]);
+
+//Agregemos nuevos valores a 1 arreglo
+autos. push( ' Audi ' ) ;// Agregamos el elemento a1 final del arreglo
+console. log (autos) ;
+
+//Otras formas de agregar elementos a1 arreglo
+autos [autos.length] = 'Porche';
+console.log(autos) ;
+
+// Tercera forme de agregar elementos teniendo CUIDADO
+autos [6] = 'Renault';
+console.log(autos) ;
