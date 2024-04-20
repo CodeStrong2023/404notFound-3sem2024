bba0dbed8646cbf9fcca2125ec2b240666b5d33e
Matias Landi <landi.matias7@gmail.com>
2024-04-20 19:52:27 -0300

    Agrego variables clase y métodos estáticos Python

--- python/semana 1/Leccion10/MiClase.py
+++ python/semana 1/Leccion10/MiClase.py
@@ -5,6 +5,10 @@ class MiClase:
     def __init__(self, variable_instancia): # La variable de instancia, da diferentes valores
         self.variable_instancia = variable_instancia
 
+        @staticmethod
+        def metodo_estatico():  # Método estático, se asocia a la clase
+            print(MiClase.variable_clase)
+
 
 print(MiClase.variable_clase)
 miClase1 = MiClase("Esta es una variable de instancia")
@@ -13,3 +17,10 @@ print(miClase1.variable_clase)
 miclase2 = MiClase("Esta es otra prueba de variable de instancia")
 print(miclase2.variable_instancia)
 print(miclase2.variable_clase)
+
+MiClase.variable_clase2 = 'Valor de variable de clase 2'
+print(MiClase.variable_clase2)
+print(miClase1.variable_clase2)
+print(miClase2.variable_clase2)
+
+MiClase.metodo_estatico()
