7aa15a10e2555be7d7cc8780cb7a13c7f1ee38fa
Gonzalo Balmaceda <www.gonzalobalmaceda@gmail.com>
2024-04-20 19:13:23 -0300

    AutoBoxing y UnBoxing parte 1

---
+++ Java/semana 3/AutoBoxingUnBoxing/src/test/TestAutoBoxingUnBoxing.java
@@ -0,0 +1,17 @@
+package test;
+
+public class TestAutoBoxingUnBoxing {
+
+    public static void main(String[] args) {
+        
+        // Clases envolventes o Wrapper.
+
+            // Clases envolventes de tipos primitivos.
+            // int = la clase envolvente es Integer.
+
+        int enteroPrim = 10; // Tipo primitivo.
+        System.out.println("enteroPrim = " + enteroPrim);
+        Integer entero = 10; // Tipo object con la clase Integer.
+        System.out.println("entero = " + entero); 
+    }
+}
