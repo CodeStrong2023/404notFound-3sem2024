8f8e62ba299ea2971c88a6e59b9d74d1b96e21c6
celeste pulitta <136055066+celestepulitta2023@users.noreply.github.com>
2024-04-20 11:43:31 -0300

    Atributo Read-only y método mro

--- python/semana 1/Leccion9/test_figura_geometrica.py
+++ python/semana 1/Leccion9/test_figura_geometrica.py
@@ -25,4 +25,6 @@ rectangulo1.ancho = 8
 print(f"Cálculo del área del rectangulo: {rectangulo1.calcular_area()}")
 print(rectangulo1)
 
-figura1 = FiguraGeometrica()
+#figura1 = FiguraGeometrica()
+# Mro significa: method resolution order
+print(Rectangulo.mro())
