b87066fee45a4f1c19027122635864541ee74c53
IGNACIO JOSE ROCHA <ignac1997nacho@gmail.com>
2024-04-20 13:33:30 -0300

    agrego la parte de java de la semana 3 video 2

--- Java/semana 3/ForEach/src/test/TestForEach.java
+++ Java/semana 3/ForEach/src/test/TestForEach.java
@@ -1,10 +1,15 @@
 package test;
-
+import domain.Persona;
 public class TestForEach {
     public static void main(String[] args) {
         int edades[] = {5, 6, 8, 9}; //sintaxis resumida
         for (int edad: edades) { //Sintaxis del ForEach
             System.out.println("edad = " + edad);
         }
+        Persona personas[]={new Persona("juan"),new Persona("carla"),new Persona("beatriz")}; 
+    //forEach
+    for(Persona persona:personas){
+        System.out.println("persona = "+persona);
+    }
     }
 }
