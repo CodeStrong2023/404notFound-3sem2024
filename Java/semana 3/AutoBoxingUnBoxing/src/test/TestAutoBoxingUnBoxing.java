bb14e8994623733d4386545fa54b2ec98e676e12
Enzo <enzoastorga98@gmail.com>
2024-04-21 14:55:50 -0300

    Autoboxing Unboxing p2/Modificadores de acceso public p1 y p2

--- Java/semana 3/AutoBoxingUnBoxing/src/test/TestAutoBoxingUnBoxing.java
+++ Java/semana 3/AutoBoxingUnBoxing/src/test/TestAutoBoxingUnBoxing.java
@@ -1,17 +1,29 @@
 package test;
 
 public class TestAutoBoxingUnBoxing {
-
     public static void main(String[] args) {
+        // Clases envolventes o Wrapper 
+        /*
+            Clases envolventes de tipo primitivos:
         
-        // Clases envolventes o Wrapper.
-
-            // Clases envolventes de tipos primitivos.
-            // int = la clase envolvente es Integer.
-
-        int enteroPrim = 10; // Tipo primitivo.
+            int = La clase envolvente Integer.
+            long = La clase envolvente Long.
+            float = La clase envolvente Float.
+            double = La clase envolvente Double.
+            boolean = La clase envolvente Boolean.
+            byte = La clase envolvente Byte.
+            char = La clase envolvente Char.
+            short = La clase envolvente Short.
+        */
+        
+        int enteroPrim = 10; // Tipo Primitivo.
         System.out.println("enteroPrim = " + enteroPrim);
-        Integer entero = 10; // Tipo object con la clase Integer.
-        System.out.println("entero = " + entero); 
+        Integer entero = 10; // Tipo Object con la clase Integer.
+        System.out.println("entero = " + entero.doubleValue()); // Autoboxing: una literal primitiva de tipo entera se convierte a otro tipo.
+        
+        // Unboxing: se extrae la literal de entero y se asigna a entero2 y el valor de entero que era un objeto pasa a estar dentro de una variable primitiva.
+        int entero2 = entero;
+        System.out.println("entero2 = " + entero2);
+        
     }
 }
