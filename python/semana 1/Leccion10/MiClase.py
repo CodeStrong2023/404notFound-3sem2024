032398520bca9f6a50a2ade14c1604c7288b5300
Javier <javii07bj@gmail.com>
2024-04-20 23:13:39 -0300

    Agrego Parte 3 y 4 Leccion 10

--- python/semana 1/Leccion10/MiClase.py
+++ python/semana 1/Leccion10/MiClase.py
@@ -5,18 +5,26 @@ class MiClase:
     def __init__(self, variable_instancia): # La variable de instancia, da diferentes valores
         self.variable_instancia = variable_instancia
 
-        @staticmethod
-        def metodo_estatico():  # Método estático, se asocia a la clase
-            print(MiClase.variable_clase)
+    @staticmethod
+    def metodo_estatico():  # Método estático, se asocia a la clase
+        print(MiClase.variable_clase)
+    @classmethod
+    def metodo_clase(cls):
+        print(cls.variable_clase)
 
+    def metodo_instancia(self):
+        self.metodo_clase()
+        self.metodo_estatico()
+        print(self.variable_clase)
+        print(self.variable_instancia)
 
 print(MiClase.variable_clase)
 miClase1 = MiClase("Esta es una variable de instancia")
 print(miClase1.variable_instancia)
 print(miClase1.variable_clase)
-miclase2 = MiClase("Esta es otra prueba de variable de instancia")
-print(miclase2.variable_instancia)
-print(miclase2.variable_clase)
+miClase2 = MiClase("Esta es otra prueba de variable de instancia")
+print(miClase2.variable_instancia)
+print(miClase2.variable_clase)
 
 MiClase.variable_clase2 = 'Valor de variable de clase 2'
 print(MiClase.variable_clase2)
@@ -24,3 +32,8 @@ print(miClase1.variable_clase2)
 print(miClase2.variable_clase2)
 
 MiClase.metodo_estatico()
+
+MiClase.metodo_clase()
+miObjeto1 = MiClase("variable de instancia")
+miObjeto1.metodo_clase()
+miObjeto1.metodo_instancia()
