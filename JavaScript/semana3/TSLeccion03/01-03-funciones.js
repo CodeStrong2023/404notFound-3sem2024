81007391552cf180be48ee75b5c515ea1a8b163a
IGNACIO JOSE ROCHA <ignac1997nacho@gmail.com>
2024-04-23 11:23:30 -0300

    agrego los videos 1 y 2 de javascript

---
+++ JavaScript/semana3/TSLeccion03/01-03-funciones.js
@@ -0,0 +1,11 @@
+miFunction(8,2); //esto se le conoce como hosting
+function miFunction(a,b){
+    //console.log("sumamos: "+(a+b));
+    return a+b;
+}
+
+//llamamos la funcion
+miFunction(3,5);
+
+let resultado=miFunction(6,7);
+console.log(resultado);
