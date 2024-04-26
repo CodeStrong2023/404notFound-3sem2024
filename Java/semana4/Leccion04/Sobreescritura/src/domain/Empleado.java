692da7403af205a9e9ab6fa6d5d0c0216af31607
Enzo <enzoastorga98@gmail.com>
2024-04-26 19:22:50 -0300

    Polimorfismo parte 1 y parte 2(Maximiliano Robilotta parte 2)

--- Java/semana4/Leccion04/Sobreescritura/src/domain/Empleado.java
+++ Java/semana4/Leccion04/Sobreescritura/src/domain/Empleado.java
@@ -7,7 +7,28 @@ public class Empleado {
         this.nombre=nombre;
         this.sueldo=sueldo;
     }
+    
+    // Método para sobreescritura
     public String obtenerDetalles(){
         return "nombre: "+this.nombre+", sueldo: "+this.sueldo;
     }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return this.sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+    
+    
+    
 }
