00e74f589db56c5d3eb875bd37161c9c7bb164c7
Gonzalo Balmaceda <www.gonzalobalmaceda@gmail.com>
2024-04-26 19:43:11 -0300

    clase Orden parte 2

--- python/Semana 3/Leccion11/Orden.py
+++ python/Semana 3/Leccion11/Orden.py
@@ -7,3 +7,28 @@ class orden:
 
     def agregar_productos(self, producto):
         self._productos.append(producto)
+
+    def calcular_total(self):
+        total = 0 # Variable temporal.
+        for producto in self._productos:
+            total += producto.precio
+        return total
+    
+    def __str__(self):
+        producto_str = ""
+        for producto in self._productos:
+            producto_str += producto.__str__() + " | "
+            
+        return f"ID orden: {self._id_orden}, \nProducto: {producto_str}"
+    
+    
+if __name__ == "__main__": # Solo sera visible si se ejecuta desde aqui.
+    producto1 = Producto("Camiseta", 5000)
+    producto2 = Producto("Pantalon", 12000)
+        
+    # Lista de productos
+    productos1 = [producto1, producto2]
+    
+    # Primer objeto de la clase 'Orden' y le pasamos la lista de productos.
+    orden1 = Orden(productos1)
+    print(orden1)
