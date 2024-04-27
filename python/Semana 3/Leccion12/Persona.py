46a72e45011d29cba1dbdfd80679337c0de8b2de
Guillermo Giannone <guillegiannone@gmail.com>
2024-04-27 02:56:45 -0300

    Agrego Sobrecarga de Operadores: Parte 2b

--- python/Semana 3/Leccion12/Persona.py
+++ python/Semana 3/Leccion12/Persona.py
@@ -1,1 +1,14 @@
 class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def __add__(self, other):  # Other significa otro
+        return f"{self.nombre} {other.nombre}"
+
+
+persona1 = Persona("Ariel", 40)
+persona2 = Persona("Betancud", 5)
+
+# persona1.__add__(persona2)   Sintaxis interna y automatica
+print(persona1 + persona2)
