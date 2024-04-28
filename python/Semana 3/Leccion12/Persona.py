1ef35754a857da13bc14f793261d32ac4c9bf315
Matias Landi <landi.matias7@gmail.com>
2024-04-28 19:03:39 -0300

    Agrego mi parte sobrecarga de operadores Python

--- python/Semana 3/Leccion12/Persona.py
+++ python/Semana 3/Leccion12/Persona.py
@@ -6,9 +6,13 @@ class Persona:
     def __add__(self, other):  # Other significa otro
         return f"{self.nombre} {other.nombre}"
 
+    def __sub__(self, otro):
+        return self.edad - otro.edad
+
 
 persona1 = Persona("Ariel", 40)
 persona2 = Persona("Betancud", 5)
 
 # persona1.__add__(persona2)   Sintaxis interna y automatica
 print(persona1 + persona2)
+print(persona1 - persona2)
