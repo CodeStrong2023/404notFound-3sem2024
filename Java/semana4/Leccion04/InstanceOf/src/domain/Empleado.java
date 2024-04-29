589091955cc6ed69b1d830a62f3cb71f46db1efc
celeste pulitta <136055066+celestepulitta2023@users.noreply.github.com>
2024-04-29 14:14:48 -0300

    Agrego Instance Of

--- Java/semana4/Leccion04/InstanceOf/src/domain/Empleado.java
+++ Java/semana4/Leccion04/InstanceOf/src/domain/Empleado.java
@@ -1,19 +1,20 @@
 
 package domain;
 
+
 public class Empleado {
-        protected String nombre;
-        protected double sueldo;
-        
-        public Empleado(String nombre, double sueldo){
-            this.nombre = nombre;
-            this.sueldo = sueldo;
-            
-        }
-        //Metodo para la sobreescritura
-        public String obtenerDetalles(){
-            return "Nombre: "+this.nombre+" Sueldo: "+this.sueldo;
-        }
+    protected String nombre;
+    protected double sueldo;
+    
+    public Empleado(String nombre, double sueldo){
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+    
+    //Metodo para la sobreescritura
+    public String obtenerDetalles(){
+        return "Nombre: "+this.nombre+", Sueldo: "+this.sueldo;
+    }
 
     public String getNombre() {
         return nombre;
@@ -30,6 +31,5 @@ public class Empleado {
     public void setSueldo(double sueldo) {
         this.sueldo = sueldo;
     }
-        
-        
+    
 }
