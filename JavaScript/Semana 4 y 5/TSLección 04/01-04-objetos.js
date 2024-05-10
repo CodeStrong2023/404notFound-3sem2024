f502a447c67891da29f64bba2ea97af97497ce5f
Javier <javii07bj@gmail.com>
2024-05-10 18:06:59 -0300

    Agrego distintas formas de crear objetos en JS

--- JavaScript/Semana 4 y 5/TSLección 04/01-04-objetos.js
+++ JavaScript/Semana 4 y 5/TSLección 04/01-04-objetos.js
@@ -90,3 +90,23 @@ console.log(padre.nombreCompleto()) //Utilizamos la función
 let madre = new Persona3('Laura', 'Contrera', 'contreral@gmail.com');
 console.log(madre);
 console.log(madre.nombreCompleto());
+
+let miObjeto = new Object();
+let miObjeto2 = {};
+
+let miCadena1 = new String("Hola");
+let miCadena2 = "Hola";
+
+let miNumero = new Number(1);
+
+let miNumero2 = 1;
+
+let miBoolean2 = false;
+
+let miArreglo1 = new Array();
+
+let miArreglo2 = [];
+
+let miFuncion1 = new function(){};
+
+let miFuncion2 = function(){};
