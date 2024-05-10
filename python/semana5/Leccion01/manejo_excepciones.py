e922884e32c2da9deb5e3d1e12cde44a147ab629
Diego Rafael Ramos Llanos <dr0573131@gmail.com>
2024-05-10 20:21:13 -0300

    se agrega Más de procedimientos de excepciones

--- python/semana5/Leccion01/manejo_excepciones.py
+++ python/semana5/Leccion01/manejo_excepciones.py
@@ -1,7 +1,8 @@
 resultado = None
-a = 7
-b = 0
+
 try:
+    a = int(input("Digite el primer numero: "))
+    b = int(input("Digite el segundo numero: "))
     resultado = a / b  # modificamos.
 except TypeError as e:
     print(f'TypeError - Ocurrió un error:  {type(e)}')
