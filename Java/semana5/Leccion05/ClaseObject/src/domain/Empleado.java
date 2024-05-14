7e11553b8611198c55c92ae22f172a2ee161096f
Rafael Ramos Llanos <dr0573131@gmail.com>
2024-05-14 18:21:47 -0300

    se agrega de java Hashcode y equals

--- Java/semana5/Leccion05/ClaseObject/src/domain/Empleado.java
+++ Java/semana5/Leccion05/ClaseObject/src/domain/Empleado.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public class Empleado {
     protected String nombre;
     protected double sueldo;
@@ -33,5 +35,33 @@ public class Empleado {
         return "Empleado{" + "nombre=" + nombre + ", sueldo=" + sueldo + '}';
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.nombre);
+        hash = 67 * hash + (int) (Double.doubleToLongBits(this.sueldo) ^ (Double.doubleToLongBits(this.sueldo) >>> 32));
+        return hash;
+    }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Empleado other = (Empleado) obj;
+        if (Double.doubleToLongBits(this.sueldo) != Double.doubleToLongBits(other.sueldo)) {
+            return false;
+        }
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        return true;
+    }
+    
 }
