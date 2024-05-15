0c0353b280e439a2fa9ec5f8ea25a9d40c1c74cd
Enzo <enzoastorga98@gmail.com>
2024-05-15 16:33:29 -0300

    Lectura de archivos p2 video 5, p3 video 6, p4 video 7

--- python/semana6/Archivos/Leccion02/leer_archivo.py
+++ python/semana6/Archivos/Leccion02/leer_archivo.py
@@ -1,2 +1,9 @@
-archivo = open('prueba.txt', 'r', encoding='UTF8')
-print(archivo.read())
+archivo = open('prueba.txt', 'r',
+               encoding='UTF8')  # Las letras son: 'r' read, 'a' append, 'w' write, 'x'
+# print(archivo.read())
+# print(archivo.read(16))
+# print(archivo.read(10))  # Continuamos desde la línea anterior
+print(archivo.readline())
+print(archivo.readline())
+
+
