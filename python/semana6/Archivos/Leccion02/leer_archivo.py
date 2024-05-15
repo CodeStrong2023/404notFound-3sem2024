91b527f482a406fd93f021af0c5b3cf437e91dee
celeste pulitta <136055066+celestepulitta2023@users.noreply.github.com>
2024-05-15 19:09:37 -0300

    Agrego fromas de trabajar con archivos

--- python/semana6/Archivos/Leccion02/leer_archivo.py
+++ python/semana6/Archivos/Leccion02/leer_archivo.py
@@ -9,4 +9,12 @@ archivo = open('prueba.txt', 'r',
 # Vamos a iterar el archivo, cada una de las líneas
 # for línea in archivo:
     # print(linea): iteramos todos los elementos del archivo
-print(archivo.readlines()[11])  # Accedemos al archivo como si fuera una lista.
+#print(archivo.readlines()[11])  # Accedemos al archivo como si fuera una lista.
+
+#Anexamos informacion, copiamos a otro
+archivo2 = open('copia.txt', 'w', encoding= 'utf8')
+archivo2.write(archivo.read())
+archivo.close() #Cerramos el primer archivo
+archivo2.close() #Cerramos el segundo archivo
+
+print('Se ha terminado el proceso de leer y copiararchivos')
