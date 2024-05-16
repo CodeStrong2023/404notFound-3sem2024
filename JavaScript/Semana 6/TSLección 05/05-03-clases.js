68a4dc8e3991c37a4499b234a2e8c464c6823d5f
Matias Landi <landi.matias7@gmail.com>
2024-05-16 08:37:36 -0300

    Agrego método get y set JavaScript

--- JavaScript/Semana 6/TSLección 05/05-03-clases.js
+++ JavaScript/Semana 6/TSLección 05/05-03-clases.js
@@ -1,11 +1,38 @@
 class Persona{
     constructor(nombre, apellido){
-        this.nombre = nombre;
-        this.apellido = apellido;
+        this._nombre = nombre;
+        this._apellido = apellido;
     }
+
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    get apellido(){
+        return this._apellido
+    }
+
+    set apellido(apellido){
+        this._apellido = apellido
+    }
+
 }
 
 let persona1 = new Persona('Martín', 'Perez');
-console.log(persona1);
+console.log(persona1.nombre)
+persona1.nombre = 'Juan Carlos'
+console.log(persona1.nombre);
+//console.log(persona1);
 let persona2 = new Persona('Carlos', 'Lara');
-console.log(persona2);
+console.log(persona2.nombre)
+persona2.nombre = 'María Laura';
+console.log(persona2.nombre)
+//console.log(persona2);
+persona1.apellido = 'Gómez'
+console.log(persona1.apellido)
+persona2.apellido = 'García'
+console.log(persona2.apellido)
