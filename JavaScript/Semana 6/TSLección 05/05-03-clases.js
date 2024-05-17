9e21f463dddc48f507edc060c1e7a8077b3dc1b2
Guillermo Giannone <guillegiannone@gmail.com>
2024-05-16 22:18:33 -0300

    Agrego Herencias parte 1 y 2 JavaScript

--- JavaScript/Semana 6/TSLección 05/05-03-clases.js
+++ JavaScript/Semana 6/TSLección 05/05-03-clases.js
@@ -1,6 +1,6 @@
 //let persona3 = new Persona("Carla", "Ponce"), esto no se debe hacer
 
-class Persona{
+class Persona{ //Clase padre
     constructor(nombre, apellido){
         this._nombre = nombre;
         this._apellido = apellido;
@@ -21,7 +21,18 @@ class Persona{
     set apellido(apellido){
         this._apellido = apellido;
     }
-
+}
+class Empleado extends Persona{ //Clase hija
+    constructor(nombre, apellido, departamento){
+        super(nombre, apellido);
+        this._departamento = departamento;
+    }
+    get departamento(){
+        return this._departamento
+    }
+    set departamento(departamento){
+        this._departamento = departamento;
+    }
 }
 
 let persona1 = new Persona('Martín', 'Perez');
@@ -38,3 +49,7 @@ persona1.apellido = 'Gómez'
 console.log(persona1.apellido)
 persona2.apellido = 'García'
 console.log(persona2.apellido)
+
+let empleado1 = new Empleado("Maria", "Gimenez", "Sistemas");
+console.log(empleado1);
+console.log(empleado1.nombre);
