58b8816c093e034943b4addf09409bf92171f35b
Diego Rafael Ramos Llanos <dr0573131@gmail.com>
2024-05-23 19:12:59 -0300

    video 5 Comenzamos con las pruebas ingresando las otras clases, parte 1

--- python/Semana 7/Leccion03/catalogo_peliculas/test_catalogo_peliculas.py
+++ python/Semana 7/Leccion03/catalogo_peliculas/test_catalogo_peliculas.py
@@ -7,6 +7,14 @@ while opcion != 4:
         print("3. Eliminar catàlogo de peliculas")
         print("4. Salir")
         opcion = int(input("Digite una opción de menú (1-4)"))
+        if opcion == 1:
+            nombre_pelicula=input("Digite el nombre de la pelicula: ")
+            pelicula = Pelicula(nombre_pelicula)
+            cp.agregar_peliculas(pelicula)
+        elif opcion == 2:
+            cp.lista_peliculas()
+        elif opcion == 3:
+        cp.eliminar_peliculas()
     except Exception as e:
         print(f"Ocurrió un error de tipo {e}")
         opcion = None
