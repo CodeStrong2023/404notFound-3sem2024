7f9e2f38d60db69d7d6d44acebfd670bdbb82fb2
Gonzalo Balmaceda <www.gonzalobalmaceda@gmail.com>
2024-05-30 13:19:32 -0300

    Manejo de excepciones parte 2

--- Java/Semana 8/Leccion 08/ManejoExcepciones/src/test/TestExcepciones.java
+++ Java/Semana 8/Leccion 08/ManejoExcepciones/src/test/TestExcepciones.java
@@ -2,8 +2,15 @@ package test;
 
 public class TestExcepciones {
     public static void main(String[] args) {
-        int resultado=10/0;
-        System.out.println("resultado= "+resultado);
+        int resultado = 0;
+        try {
+            resultado = 10/0;
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error");
+            e.printStackTrace(System.out); // Se conoce como la pila de excepciones.
+        }
+
+        System.out.println("La variable resultado tiene como valor: " + resultado);
         
     }
 }
