b1f7ee9b2512825fe356bb17e946fa7e40d875e4
Diego Rafael Ramos Llanos <dr0573131@gmail.com>
2024-06-02 20:01:50 -0300

    se sube el video 9 de java

--- Java/Semana 8/Leccion 08/ManejoExcepciones/src/test/TestExcepciones.java
+++ Java/Semana 8/Leccion 08/ManejoExcepciones/src/test/TestExcepciones.java
@@ -1,14 +1,17 @@
 package test;
 
+import aritmetica.Aritmetica;
+
 public class TestExcepciones {
     public static void main(String[] args) {
         int resultado = 0;
-        //try {
-            resultado = 10/0;
-//        } catch (Exception e) {
-//            System.out.println("Ocurrio un error");
-//            e.printStackTrace(System.out); // Se conoce como la pila de excepciones.
-//        }
+        try {
+            resultado = Aritmetica.division(10, 0);
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error");
+            e.printStackTrace(System.out); // Se conoce como la pila de excepciones.
+            System.out.println(e.getMessage());
+        }
 
         System.out.println("La variable resultado tiene como valor: " + resultado);
         
