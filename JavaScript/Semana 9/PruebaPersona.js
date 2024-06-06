54b719dbe14e52f9006c61a38036118b0331e004
maxi <maxirobilottapons@gmail.com>
2024-06-06 18:28:25 -0300

    Video 6 Clase 9 JS Prueba Clase Persona

--- JavaScript/Semana 9/PruebaPersona.js
+++ JavaScript/Semana 9/PruebaPersona.js
@@ -96,3 +96,9 @@ class Cliente extends Persona{
 	}
 }
 
+//Prueba clase Persona
+let persona1 = new Persona("Juan","Perez",32);
+console.log(persona1.toString()); 1 Juan Perez 32
+
+let persona2 = new Persona("Carla" ,"Ortega",22);
+console.log(persona2.toString()); 2 Carla Ortega 22
