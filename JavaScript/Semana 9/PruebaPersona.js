f1bac8baeb633e253d6dfa9f72fabe8c97f9a93b
Matias Landi <landi.matias7@gmail.com>
2024-06-06 08:37:08 -0300

    Agrego parte 5 de la clase 9 Herencia JavaScript

---
+++ JavaScript/Semana 9/PruebaPersona.js
@@ -0,0 +1,98 @@
+class Persona{
+
+    static contadorPersonas = 0;
+
+    constructor(nombre, apellido, edad){
+        this._idPersona = ++Persona.contadorPersonas;
+        this._nombre = nombre;
+        this._apellido = apellido;
+        this._edad = edad;
+    }
+
+    get idPersona(){
+        return this._idPersona
+    }
+
+    get nombre(){
+        this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    get apellido(){
+        return this._apellido
+    }
+
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
+
+    get edad(){
+        return this._edad;
+    }
+
+    set edad(edad){
+        this._edad = edad;
+    }
+
+    toString(){
+        return this._idPersona+' '+this._nombre+' '+this._apellido+' '+this._edad;
+    }
+}
+
+class Empleado extends Persona{
+    static contadorEmpleados = 0;
+
+    constructor(nombre, apellido, edad, sueldo){
+        super(nombre, apellido, edad);
+        this._idEmpleado = ++ Empleado.contadorEmpleados;
+        this._sueldo = sueldo;
+
+    }
+
+    get _idEmpleado(){
+        return this._idEmpleado;
+    }
+    get sueldo(){
+        this._sueldo;
+    }
+
+    set sueldo(sueldo){
+        this._sueldo = sueldo;
+    }
+
+    toString(){
+    return `
+    ${super.toString} 
+    ${this._idEmpleado} 
+    ${this._sueldo}`;
+    }
+}
+
+class Cliente extends Persona{
+	static contadorClientes=0;
+	constructor (nombre,apellido, edad,fechaRegistro){
+		super(nombre, apellido, edad);
+		this._idCliente =++Cliente.contadorClientes;
+		this._fechaRegistro=fechaRegistro;
+	}
+	get idCliente(){
+		return this._idCliente;
+	}
+	get fechaRegistro(){
+		return this._fechaRegistro
+	}
+	set fechaRegistro(fechaRegistro){
+		this._fechaRegistro=fechaRegistro
+	}
+	toString(){
+		return `
+			${super.toString()}
+			${this._idCliente}
+			${this._fechaRegistro}`;
+		
+	}
+}
+
