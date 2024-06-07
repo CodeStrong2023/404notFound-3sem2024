282b4b99af64c52d1e57b03bbc52a7a00f9dafc6
celeste pulitta <136055066+celestepulitta2023@users.noreply.github.com>
2024-06-07 02:23:50 -0300

    Agrego entrada de información y menú

--- Java/Semana 9/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
+++ Java/Semana 9/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
 public class CalculadoraUTN {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
         System.out.println("******* Aplicacion Calculadora *******");
-        var operando1 =10;
-        var operando2 =20;
-        var resultado = operando1 + operando2;
-        System.out.println("resultado= "+resultado );
+        //Mostramos el menú
+        System.out.println("""
+                1. Suma
+                2. Resta
+                3. Multiplicacion
+                4. Division
+                5. Salir
+                """);
+        System.out.print("Operación a realizar? ");
+        var operacion = Integer.parseInt(entrada.nextLine());
+
+        if(operacion >= 1 && operacion <= 4){
+            System.out.println("Digite el valor para el operando1: ");
+            var operando1 =Integer.parseInt(entrada.nextLine());
+            System.out.println("Digite el valor para el operando2: ");
+            var operando2 =Integer.parseInt(entrada.nextLine());
+
+        }
 
     }
 }
