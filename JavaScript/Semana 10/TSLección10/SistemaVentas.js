33965ed57be447b198788073a052a64cabcfbf1d
Enzo <enzoastorga98@gmail.com>
2024-06-11 15:53:12 -0300

    Video 4 - Creación clase Orden - Clase 10 JS

--- JavaScript/Semana 10/TSLección10/SistemaVentas.js
+++ JavaScript/Semana 10/TSLección10/SistemaVentas.js
@@ -32,6 +32,34 @@ class Producto{
 }//Fin de la clase Producto
 
 
+class Orden{
+    static contadorOrdenes = 0;
+    static getMAX_PRODUCTOS(){
+        return 5;
+    }
+
+    constructor(){
+        this._idOrden = ++Orden.contadorOrdenes;
+        this._productos = [];
+        this._contadorProductosAgregados = 0;
+    }
+
+    get idOrden(){
+        return this._idOrden;
+    }
+
+    agregarProducto(producto){
+        if(this._productos.lenght < Orden.getMAX_PRODUCTOS()){
+            this._productos.push(producto); // Tenemos 2 tipos de sintaxis: 1
+            //this._productos[this._contadorProductosAgregados++] = producto; // Segunda sintaxis
+        }
+        else{
+            console.log('No se pueden agregar más productos')
+        }
+    } // Fin del método agregarProducto
+} // Fin de la clase Orden
+
+
 let producto1 = new Producto('Pantalón', 200);
 let producto2 = new Producto('Camisa', 150);
 console.log(producto1.toString());
