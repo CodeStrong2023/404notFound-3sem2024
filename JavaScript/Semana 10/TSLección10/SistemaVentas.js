71dee4b0bc57c5cbd17770851a7a6015fd1468f4
celeste pulitta <136055066+celestepulitta2023@users.noreply.github.com>
2024-06-11 17:34:40 -0300

    Pruebas con ralación de agregación

--- JavaScript/Semana 10/TSLección10/SistemaVentas.js
+++ JavaScript/Semana 10/TSLección10/SistemaVentas.js
@@ -34,7 +34,7 @@ class Producto{
 
 class Orden{
     static contadorOrdenes = 0;
-    static getMAX_PRODUCTOS(){
+    static getMAX_PRODUCTOS(){//Simula una constante
         return 5;
     }
 
@@ -57,10 +57,36 @@ class Orden{
             console.log('No se pueden agregar más productos')
         }
     } // Fin del método agregarProducto
+
+    calcaluarTotal(){
+        let totalVenta = 0;
+        for(let producto of this._productos){
+            totalVenta += producto.precio;
+        }//Fin ciclo for
+        return totalVenta;
+    }//Fin del método calcalarTotal
+
+    mostrarOrden(){
+        let productosOrden = '';
+        for(let producto of this._productos){
+            productosOrden += '\n{' +producto.toString()+' }';
+        }//Fin del ciclo for
+        console.log('Orden: ${this.idOrden}, Total: $${this.calcalarTotal()}, Productos: ${productosOrden}');
+    }//Fin del método mostrarOrden
+    
 } // Fin de la clase Orden
 
 
 let producto1 = new Producto('Pantalón', 200);
 let producto2 = new Producto('Camisa', 150);
-console.log(producto1.toString());
-console.log(producto2.toString());
+let producto3 = new Producto('Cinturon', 50);
+let orden1 = new Orden();
+let orden2 = new Orden();
+orden1.agregarProducto(producto1);
+orden1.agregarProducto(producto2);
+orden1.agregarProducto(producto3);
+orden1.agregarProducto(producto1);
+orden1.agregarProducto(producto2);
+orden2.agregarProducto(producto3);
+orden1.mostrarOrden();
+orden2.mostrarOrden();
