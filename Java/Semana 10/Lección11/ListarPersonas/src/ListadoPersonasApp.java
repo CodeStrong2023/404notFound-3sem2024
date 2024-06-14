41bac7eb1dd0976aac833436c870c598b76d8564
celeste pulitta <136055066+celestepulitta2023@users.noreply.github.com>
2024-06-14 00:16:12 -0300

    Agregamos creación de la clase persona parte 3 y menú parte 1

---
+++ Java/Semana 10/Lección11/ListarPersonas/src/ListadoPersonasApp.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ListadoPersonasApp {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        //Definimos la lista fuera del ciclo while
+        List<Persona> personas = new ArrayList<>();
+        // Empezamos con el menú
+        var salir = false;
+        while(!salir){
+            mostrarMenu();
+        }//Fin de ciclo while
+    }//Fin método main
+}
