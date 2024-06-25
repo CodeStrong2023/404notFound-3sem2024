9d65a8cf9e3d4b065729ad4c63d28caefd60bdb3
IGNACIO JOSE ROCHA <ignac1997nacho@gmail.com>
2024-06-25 09:04:54 -0300

    agrego los videos 1 y 2 de python

--- python/Semana 9/Leccion07/capa_datos_persona/persona_dao.py
+++ python/Semana 9/Leccion07/capa_datos_persona/persona_dao.py
@@ -1,3 +1,6 @@
+from capa_datos_persona.conexion import Conexion
+from capa_datos_persona.Persona import Persona
+from logger_base import log
 class PersonaDAO:
     """
     DAO significa: Data Access Objet
@@ -12,3 +15,20 @@ class PersonaDAO:
     _INSERTAR ='INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
     _ACTUALIZAR = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
     _ELIMINAR = 'DELETE FROM persona WHERE id_persona=%s'
+
+    #definimos los metodos de clase
+    @classmethod
+    def seleccionar(cls):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                cursor.execute(cls._SELECCIONAR)
+                registros=cursor.fetchall()
+                personas=[]#creamos una lista
+                for registro in registros:
+                    persona= Persona(registro[0], registro[1], registro[2], registro[3])
+                    personas.append(persona)
+
+if __name__=='__main__':
+    personas=PersonaDAO.seleccionar()
+    for persona in personas:
+        log.debug(persona)
