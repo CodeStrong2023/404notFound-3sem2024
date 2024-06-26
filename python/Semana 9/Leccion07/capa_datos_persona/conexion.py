79a1871db74212140a93f2851ddfd1e22962a473
Javier <javii07bj@gmail.com>
2024-06-26 00:49:46 -0300

    Agrego parte pool de conexiones de python

--- python/Semana 9/Leccion07/capa_datos_persona/conexion.py
+++ python/Semana 9/Leccion07/capa_datos_persona/conexion.py
@@ -43,12 +43,25 @@ class Conexion:
         else:
             return cls._pool
 
+    @classmethod
+    def liberarConexion(cls, conexion):
+        cls.obtenerPool().putconn(conexion)
+        log.debug(f'Regresamos la coneccion del pool: {conexion}')
+
+    @classmethod
+    def cerrarConeiones(cls):
+        cls.obtenerPool().closeall()
+
 
 if __name__ == "__main__":
     conexion1 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion1)
     conexion2 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion2)
     conexion3 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion3)
     conexion4 = Conexion.obtenerConexion()
     conexion5 = Conexion.obtenerConexion()
+    conexion6 = Conexion.obtenerConexion()
 
 
