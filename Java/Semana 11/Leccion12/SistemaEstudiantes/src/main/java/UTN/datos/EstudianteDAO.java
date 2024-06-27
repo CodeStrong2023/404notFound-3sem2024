961a006555143f7b33fe4977744766e5026ecc15
celeste pulitta <136055066+celestepulitta2023@users.noreply.github.com>
2024-06-27 05:08:24 -0300

    Creamos la clase entidad estudiante y listado estudiantes con la clase EstudianteDao parte1 java

---
+++ Java/Semana 11/Leccion12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -0,0 +1,19 @@
+package UTN.datos;
+
+import UTN.dominio.Estudiante;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstudianteDAO {
+    // Método listar
+    public List<Estudiante> listar(){
+        List<Etudiante> estudiantes = new ArrayList<>()
+        // Creamos algunos objetos que son ncesarios para comunicarnos con la base de datos
+        PreparedStatement ps; //Envia la sentencia a la base de datos
+        ResultSet rs; //Obtenemos el resultado de la base de datos
+
+    }
+}
