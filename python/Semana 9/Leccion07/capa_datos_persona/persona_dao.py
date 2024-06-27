5ba790ec1773833e2bba65302951154da4f4b765
Guillermo Giannone <guillegiannone@gmail.com>
2024-06-27 00:34:49 -0300

    Pruebas de personaDAO -> Parte 1 y 2 Python

--- python/Semana 9/Leccion07/capa_datos_persona/persona_dao.py
+++ python/Semana 9/Leccion07/capa_datos_persona/persona_dao.py
@@ -58,19 +58,19 @@ class PersonaDAO:
 
 if __name__=='__main__':
     # Eliminar un registro
-    # persona1 = Persona(id_persona=8)
-    # personas_eliminadas = PersonaDAO.eliminar(persona1)
-    # log.debug(f'Personas eliminadas: {personas_eliminadas}')
+    persona1 = Persona(id_persona=8)
+    personas_eliminadas = PersonaDAO.eliminar(persona1)
+    log.debug(f'Personas eliminadas: {personas_eliminadas}')
 
     # Actualizar un registro
-    # persona1 = Persona(1, 'Juan José', 'Pena', 'jjpena@mail.com')
-    # personas_actualizadas = PersonaDAO.actualizar(persona1)
-    # log.debug(f'Personas actucalizadas: {personas_actualizadas}')
+    persona1 = Persona(1, 'Juan', 'Pena', 'jpena@mail.com')
+    personas_actualizadas = PersonaDAO.actualizar(persona1)
+    log.debug(f'Personas actucalizadas: {personas_actualizadas}')
 
     # Insertar registro
-    # persona1 = Persona(nombre='Omero', apellido='Ramos', email='omeror@mail.com')
-    # personas_insertadas = PersonaDAO.insertar(persona1)
-    # log.debug(f'Personas inseradas: {personas_insertadas}')
+    persona1 = Persona(nombre='Mateo', apellido='Torres', email='tmateo@mail.com')
+    personas_insertadas = PersonaDAO.insertar(persona1)
+    log.debug(f'Personas inseradas: {personas_insertadas}')
 
     # Seleccionar objetos
     personas = PersonaDAO.seleccionar()
